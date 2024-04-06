28e6fe0b1281040704d87a49bbfab6c51f2fcbad
chetan <chetansharma911912@gmail.com>
2024-04-06 18:55:09 +0530

    main page changed

--- js/actions.js
+++ js/actions.js
@@ -2,3 +2,15 @@
 function gotoPage(url){
     window.location.href = url+".html"
 }
+
+
+function loadblock(blockname){
+    if(blockname=="login"){
+        document.querySelector("#login").style.display = "block";
+        document.querySelector("#signup").style.display = "none";
+    }
+    else if(blockname=="signup"){
+        document.querySelector("#login").style.display = "none";
+        document.querySelector("#signup").style.display = "block";
+    }
+}
