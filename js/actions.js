f9edb39cfb82f569bc80c7ae76da7871295ca6da
chetan <chetansharma911912@gmail.com>
2024-04-07 01:03:09 +0530

    some files are removed

--- js/actions.js
+++ js/actions.js
@@ -1,4 +1,18 @@
 
+
+window.onresize = ()=>{
+    checkScreensize();
+}
+
+function checkScreensize(){
+    if(window.screen.width>570 && !(window.location.href.includes("error.html"))){
+        window.location.href = "error.html"
+    }
+    else if(window.screen.width<570 && (window.location.href.includes("error.html"))){
+        window.location.href = "index.html"
+    }
+}
+
 function gotoPage(url){
     window.location.href = url+".html"
 }
@@ -14,3 +28,60 @@ function loadblock(blockname){
         document.querySelector("#signup").style.display = "block";
     }
 }
+
+
+window.onload=()=>{
+    checkScreensize();
+
+    let header = `
+    <nav class="navbar navbar-expand-sm">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#" onclick="gotoPage('index')">
+              <!-- <b>Couple Game</b> -->
+              <img class="animate__animated animate__bounceIn" src="./images/logo_1.PNG" width="70">
+              <b>Couple's For.Play</b>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+              <span class="navbar-toggler-icon">menu</span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+              <button class="btn btn-success" onclick="gotoPage('index')">Home</button>
+              <!-- <button class="btn btn-danger" onclick="gotoPage('signin')">SignUp</button> -->
+              <button class="btn btn-info">About</button>
+              <button class="btn btn-warning">Contact <span
+                      style="color:red !important;">&hearts;</span></button>
+    
+              <!-- <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">About Us</a>
+                    </li>
+                  </ul> -->
+          </div>
+      </div>
+    </nav>
+    `;
+    
+    let footer = `
+    <div class="card">
+        <div class="card-body">
+            <!-- <h4 class="card-title text-center">Contact Us</h4> -->
+            <a href="#">Contact Us</a>
+            <a href="#">Terms</a>
+            <a href="#">Privacy</a>
+            <p class="card-text text-center">Made With <span style="color:red !important;">&hearts;</span> and <span
+                    style="color:red !important;">&hearts;</span></p>
+        </div>
+    </div>
+    `;
+    
+      setTimeout(() => {
+        document.querySelector("header").innerHTML = header;
+        document.querySelector("footer").innerHTML = footer;
+      }, 200);
+    }
