1b06ccd837de83b0fcec3b6459489ea0168c2ec7
chetan <chetansharma911912@gmail.com>
2024-04-14 13:42:40 +0530

    modification of files and directory

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+window.onload=()=>{
+    checkScreensize();
+}
+
+window.onresize = ()=>{
+    checkScreensize();
+}
+
+function checkScreensize(){
+    if(window.screen.width>570 && !(window.location.href.includes("error.html"))){
+        window.location.href = "error.html"
+    }
+    else if(window.screen.width<570 && (window.location.href.includes("error.html"))){
+        window.location.href = "index.html"
+    }
+}
+
+function loadblock(event,blockname){
+    if(blockname=="login"){
+        document.querySelector("#login").style.display = "block";
+        document.querySelector("#signup").style.display = "none";
+    }
+    else if(blockname=="signup"){
+        document.querySelector("#login").style.display = "none";
+        document.querySelector("#signup").style.display = "block";
+    }
+    document.querySelectorAll(".nav-link").forEach((item)=>{
+        item.classList.remove("active");
+    });
+    event.classList.add("active");
+}
