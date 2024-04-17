3d4edcd0015cab40427327565d5938594320406e
chetan <chetansharma911912@gmail.com>
2024-04-17 13:56:24 +0530

    ping pong changs

--- js/easy.js
+++ js/easy.js
@@ -1,44 +1,84 @@
 class Rectangle{
-	constructor(x,y,width,height){
+	constructor(x,y,width,height,borderColor,fillColor){
 		this.x=x;
 		this.y=y;
 		this.width=width;
 		this.height=height;
+		this.borderColor=borderColor;
+		this.fillColor=fillColor;
+		this.shape = "rect";
+	}
+}
+
+class RoundRectangle{
+	constructor(x,y,width,height,radii,borderColor,fillColor){
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+		this.radii=radii;
+		this.borderColor=borderColor;
+		this.fillColor=fillColor;
+		this.shape = "roundrect";
 	}
 }
 
 class Circle{
-	constructor(x,y,radius){
+	constructor(x,y,radius,borderColor,fillColor){
 		this.x=x;
 		this.y=y;
 		this.radius=radius;
+		this.borderColor=borderColor;
+		this.fillColor=fillColor;
+		this.shape = "circle";
 	}
 }
 
 class Ellipse{
-	constructor(x,y,width,height){
+	constructor(x,y,width,height,borderColor,fillColor){
 		this.x=x;
 		this.y=y;
 		this.width=width;
 		this.height=height;
+		this.borderColor=borderColor;
+		this.fillColor=fillColor;
+		this.shape = "ellipse";
 	}
 }
 
 class Line{
-	constructor(x,y,x2,y2){
+	constructor(x,y,x2,y2,borderColor,fillColor){
 		this.x=x;
 		this.y=y;
 		this.x2=x2;
 		this.y2=y2;
+		this.borderColor=borderColor;
+		this.fillColor=fillColor;
+		this.shape = "line";
+	}
+}
+
+class SpriteImage{
+	constructor(img,x,y,width,height){
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+		this.img= new Image();
+		this.img.src=img;
+		this.shape = "img";
 	}
 }
 
 class Text{
-	constructor(text,font,x,y){
+	constructor(text,font,x,y,borderColor,fillColor){
 		this.x=x;
 		this.y=y;
 		this.text=text;
 		this.font=font;
+		this.borderColor=borderColor;
+		this.fillColor=fillColor;
+		this.shape = "text";
 	}
 }
 
@@ -46,6 +86,7 @@ var createCanvas=function(width,height,css) {
 	this.width=width;
 	this.height=height;
 	this.css=css;
+	let shapes= [];
 	this.drawCanvas=function(){
 		let container = document.querySelector(".container");
 		this.canvas= document.createElement("CANVAS");
@@ -64,163 +105,100 @@ var createCanvas=function(width,height,css) {
 		this.context().clearRect(0,0,this.width,this.height);
 	}
 
-	this.drawRect=function(r,color,bgcolor){
+	this.draw = ()=>{
 		var ctx=this.context();
-		if(typeof r != "undefined" && typeof color != "undefined"){
-			ctx.beginPath();
-			ctx.rect(r.x,r.y,r.width,r.height);
-			ctx.strokeStyle=color;
-			ctx.closePath();
-			ctx.stroke();
-		}
-		if(typeof r != "undefined" && typeof color != "undefined" && typeof bgcolor != "undefined"){
-			ctx.beginPath();
-			ctx.strokeStyle=color;
-			ctx.fillStyle=bgcolor;
-			ctx.fillRect(r.x,r.y,r.width,r.height);
-			ctx.closePath();
-		}
-		if(typeof r != "undefined" && typeof color == "" && typeof bgcolor != "undefined"){
-			ctx.beginPath();
-			ctx.fillStyle=bgcolor;
-			ctx.fillRect(r.x,r.y,r.width,r.height);
-			ctx.closePath();
-		}
-	}	
+		ctx.clearRect(0,0,this.width,this.height);
+		shapes.forEach((item)=>{
+			item.borderColor = item.borderColor==undefined? "white" : item.borderColor;
 
-	this.drawCircle=function(c,color,bgcolor){
-		var ctx=this.context();
-		if(typeof c != "undefined" && typeof color != "undefined"){
 			ctx.beginPath();
-			ctx.strokeStyle=color;
-			ctx.arc(c.x,c.y,c.radius,0,2*Math.PI);
-			ctx.stroke();ctx.closePath();
-		}
-		if(typeof c != "undefined" && typeof color != "undefined" && typeof bgcolor != "undefined"){
-			ctx.beginPath();
-			ctx.strokeStyle=color;
-			ctx.fillStyle=bgcolor;
-			ctx.arc(c.x,c.y,c.radius,0,2*Math.PI);
-			ctx.fill();ctx.closePath();
-		}
-		if(typeof c != "undefined" && typeof color == "" && typeof bgcolor != "undefined"){
-			ctx.beginPath();
-			ctx.fillStyle=bgcolor;
-			ctx.arc(c.x,c.y,c.radius,0,2*Math.PI);
-			ctx.fill();
+			ctx.strokeStyle= item.borderColor;
+			ctx.fillStyle = item.fillColor;
+			
+			if(item.shape=="rect") {
+				if(item.fillColor==undefined){
+					ctx.rect(item.x,item.y,item.width,item.height);
+					ctx.stroke();
+				}
+				else{
+					ctx.rect(item.x,item.y,item.width,item.height);
+					ctx.stroke();
+					ctx.fillRect(item.x,item.y,item.width,item.height);
+				}
+			}
+			if(item.shape=="roundrect") {
+				ctx.roundRect(item.x,item.y,item.width,item.height,item.radii);
+				if(item.fillColor==undefined){
+					ctx.stroke();
+				}
+				else {
+					ctx.stroke();
+					ctx.fill();
+				}
+			}
+			else if(item.shape=="circle"){
+				ctx.arc(item.x,item.y,item.radius,0,2*Math.PI);
+				if(item.fillColor==undefined){
+					ctx.stroke();
+				}
+				else {
+					ctx.stroke();
+					ctx.fill();
+				}
+				
+			}
+			else if(item.shape=="line"){
+				ctx.moveTo(item.x,item.y);
+				ctx.lineTo(item.x2,item.y2);
+				ctx.stroke();
+			}
+			else if(item.shape=="text"){
+				ctx.font=item.font;
+				ctx.strokeStyle=item.borderColor;
+				ctx.strokeText(item.text,item.x,item.y);
+				ctx.stroke();
+			}
+			else if(item.shape=="ellipse"){
+				ctx.moveTo(e.x, e.y - e.height/2);
+				ctx.bezierCurveTo(e.x + e.width/2, e.y - e.height/2, e.x + e.width/2, e.y + e.height/2, e.x, e.y + e.height/2);
+				ctx.bezierCurveTo(e.x - e.width/2, e.y + e.height/2, e.x - e.width/2, e.y - e.height/2, e.x, e.y - e.height/2);
+				if(item.fillColor==undefined){
+					ctx.stroke();
+				}
+				else{
+					ctx.fill();
+				}
+			}
+			else if(item.shape=="img"){
+				ctx.drawImage(item.img, item.x, item.y,item.width,item.height);
+			}
+
 			ctx.closePath();
-		}
+		});
 	}
 
-	this.drawLine=function(l,color){
-		var ctx=this.context();
-		if(typeof r != "undefined" && typeof color != "undefined"){
-			ctx.beginPath();
-			ctx.line(l.x,l.y,l.x1,l.y1);
-			ctx.strokeStyle=color;
-			ctx.closePath();
-			ctx.stroke();
-		}
-		if(typeof r != "undefined" && typeof color != "undefined"){
-			ctx.beginPath();
-			ctx.strokeStyle=color;
-			ctx.fillStyle=bgcolor;
-			ctx.line(l.x,l.y,l.x1,l.y1);
-			ctx.closePath();
-		}
-		if(typeof r != "undefined" && typeof color == ""){
-			ctx.beginPath();
-			ctx.fillStyle=bgcolor;
-			ctx.line(l.x,l.y,l.x1,l.y1);
-			ctx.closePath();
-		}
-	}
-
-	this.drawEllipse=function(e,color,bgcolor){
-		var ctx=this.context();
-		if(typeof e != "undefined" && typeof color != "undefined"){
-			ctx.beginPath();
-			ctx.moveTo(e.x, e.y - e.height/2);
-			
-			ctx.bezierCurveTo(e.x + e.width/2, e.y - e.height/2,
-				e.x + e.width/2, e.y + e.height/2,
-				e.x, e.y + e.height/2);
-
-			ctx.bezierCurveTo(e.x - e.width/2, e.y + e.height/2,
-			    e.x - e.width/2, e.y - e.height/2,
-			    e.x, e.y - e.height/2);
-			 
-			ctx.strokeStyle = color;
-			ctx.stroke();
-			ctx.closePath();
-		}	
-		if(typeof e != "undefined" && typeof color != "undefined" && typeof bgcolor != "undefined"){
-			ctx.beginPath();
-			ctx.moveTo(e.x, e.y - e.height/2);
-			
-			ctx.bezierCurveTo(e.x + e.width/2, e.y - e.height/2,
-				e.x + e.width/2, e.y + e.height/2,
-				e.x, e.y + e.height/2);
-
-			ctx.bezierCurveTo(e.x - e.width/2, e.y + e.height/2,
-			    e.x - e.width/2, e.y - e.height/2,
-			    e.x, e.y - e.height/2);
-			 
-			ctx.strokeStyle = color;
-			ctx.stroke();
-			ctx.fillStyle=bgcolor;
-			ctx.fill();
-			ctx.closePath();
-		}
-		else if(typeof e != "undefined" && typeof color == "" && typeof bgcolor != "undefined"){
-			ctx.beginPath();
-			ctx.moveTo(e.x, e.y - e.height/2);
-			
-			ctx.bezierCurveTo(e.x + e.width/2, e.y - e.height/2,
-				e.x + e.width/2, e.y + e.height/2,
-				e.x, e.y + e.height/2);
-
-			ctx.bezierCurveTo(e.x - e.width/2, e.y + e.height/2,
-			    e.x - e.width/2, e.y - e.height/2,
-			    e.x, e.y - e.height/2);
-			 
-			ctx.fillStyle=bgcolor;
-			ctx.fill();
-			ctx.closePath();
-		}
-		
+	this.setShapes = (arr)=>{
+		shapes=arr;
 	}
 	
-	this.drawLine=function(l,color){
-		var ctx=this.context();
-		ctx.beginPath();
-		ctx.moveTo(l.x,l.y);
-		ctx.lineTo(l.x2,l.y2);
-		ctx.strokeStyle=color;
-		ctx.closePath();
-		ctx.stroke();
-	}
-
-	this.drawText=function(t,color){
-		var ctx=this.context();
-		ctx.font=t.font;
-		ctx.strokeStyle=color;
-		ctx.strokeText(t.text,t.x,t.y);
+	this.animate = (moveShapes)=>{
+		setInterval(()=>{
+			moveShapes();
+			this.draw(shapes);
+		},0);
 	}
 
 	this.setBackground=function(bgcolor){
 		this.canvas.style.background=bgcolor;
 	}
 };
-var vibrate=function(shape,two){
-	shape.x+=random(two,"-");
-	shape.y+=random(two,"-");
+var vibrate=function(shape, number){
+	shape.x+=random(number,"-");
+	shape.y+=random(number,"-");
 }
 function distCircle(c1,c2){
 	let dist = (Math.sqrt(Math.pow((c2.x-c1.x),2)+Math.pow((c2.y-c1.y),2)));
-	// console.log(dist);
 	return dist;
-	
 }
 function circleCollide(c1,c2) {
     if(distCircle(c1,c2)<(c1.radius+c2.radius)){
